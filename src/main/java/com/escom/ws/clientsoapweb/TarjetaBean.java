dbc16dde143401f9ace15cc5266eac834eb233b4
Eduardo Castillo <ecastillo@anzen.com.mx>
2020-02-27 23:04:25 -0600

    add proyecto

---
+++ src/main/java/com/escom/ws/clientsoapweb/TarjetaBean.java
@@ -0,0 +1,40 @@
+package com.escom.ws.clientsoapweb;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+
+/**
+ *
+ * @author edcastillo
+ */
+@ManagedBean
+@RequestScoped
+public class TarjetaBean {
+
+	public TarjetaBean() {
+	}
+
+	public void guardarTarjeta() {
+		
+	}
+
+	public void listarTarjetas() {		
+
+	}
+
+	public void eliminarId(String numeroTarjeta) {
+
+	}
+
+	public void buscarId() {
+
+	}
+
+	public void actualizar() {
+
+
+	}
+
+	
+}
